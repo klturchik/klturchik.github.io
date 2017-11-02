815825fc8b0e7542624ec27bbbd19179617620bd
klturchik <kturchik@sbcglobal.net>
2017-11-02 10:17:03 -0700

    Fixed navbar scroll offset

--- js/freelancer.js
+++ js/freelancer.js
@@ -8,7 +8,7 @@
       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
       if (target.length) {
         $('html, body').animate({
-          scrollTop: (target.offset().top - 48)
+          scrollTop: (target.offset().top - 0)
         }, 1000, "easeInOutExpo");
         return false;
       }
@@ -23,7 +23,7 @@
   // Activate scrollspy to add active class to navbar items on scroll
   $('body').scrollspy({
     target: '#mainNav',
-    offset: 48
+    offset: 0
   });
 
   // Collapse the navbar when page is scrolled
